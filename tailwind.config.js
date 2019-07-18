860f07a40325ee463af11bb00a2840829c3fe98e
Emilia Rubio <emilia.rubio@gmail.com>
2019-07-18 17:58:45 -0400

    Add Tailwind to Jekyll project

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  theme: {
+    extend: {
+      inset: {
+        '-full': '-100%',
+        'full': '100%',
+      }  
+    },
+    // transitionProperty: {
+    //   'left': 'left',
+    //   'right': 'right',
+    // },
+    // transitionDuration: {
+    //   '300': '300ms',
+    // },
+    // transitionTimingFunction: {
+    //   'ease': 'ease',
+    // },
+  },
+  variants: {},
+  plugins: []
+}
